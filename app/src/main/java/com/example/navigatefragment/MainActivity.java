3fa8e4cd287b2c74c53e433f24abf1214c6d91d9
LS <paytmking15@gmail.com>
2024-03-16 17:35:40 +0530

    second

--- app/src/main/java/com/example/navigatefragment/MainActivity.java
+++ app/src/main/java/com/example/navigatefragment/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.navigatefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        if (findViewById(R.id.container)!=null)
+        {
+            if (savedInstanceState!=null){
+                return;
+            }
+            FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+            First_Fragment ff =new First_Fragment();
+            fragmentTransaction.add(R.id.container,ff,null);
+            fragmentTransaction.commit();
+        }
+
+
     }
 }
